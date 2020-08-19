682d3797d7620a48d49b25ec2501db8676717bfa
lupeng <lupeng@hoge.cn>
2020-08-19 15:14:08 +0800

    add Hash

--- src/Application.php
+++ src/Application.php
@@ -19,7 +19,7 @@ use Mini\Support\Command;
 
 class Application
 {
-    public static string $version = '1.0.9';
+    public static string $version = '1.0.10';
 
     public static array $mapping = [
         'http' => HttpServer::class,
