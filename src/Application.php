8f828b29b20715ca854cf174d4b43ffb98584152
lupeng <lupeng@hoge.cn>
2020-08-27 17:01:17 +0800

    fix

--- src/Application.php
+++ src/Application.php
@@ -19,7 +19,7 @@ use Mini\Support\Command;
 
 class Application
 {
-    public static string $version = '1.0.20';
+    public static string $version = '1.0.21';
 
     public static array $mapping = [
         'http' => HttpServer::class,
