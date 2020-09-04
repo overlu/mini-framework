b7598bc434b1cc6cbb21cc8f144eb2e4c4b07e67
lupeng <lupeng@mini.com>
2020-09-04 13:37:42 +0000

    add waitgroup

---
+++ src/Support/WaitGroup.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Mini.
+ * @auth lupeng
+ */
+declare(strict_types=1);
+
+namespace Mini\Support;
+
+use Swoole\Coroutine\WaitGroup as SwooleWaitGroup;
+
+class WaitGroup extends SwooleWaitGroup
+{
+}
