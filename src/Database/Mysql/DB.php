33b39b00998c46db0dda94c3c38a12f5e665273f
lupeng <lupeng@hoge.cn>
2020-12-10 16:20:42 +0800

    优化

--- src/Database/Mysql/DB.php
+++ src/Database/Mysql/DB.php
@@ -9,7 +9,30 @@ namespace Mini\Database\Mysql;
 
 use Mini\Database\Mysql\Capsule\Manager;
 
+/**
+ * Class DB
+ * @method static \Mini\Database\Mysql\ConnectionInterface connection(string $name = null)
+ * @method static string getDefaultConnection()
+ * @method static void setDefaultConnection(string $name)
+ * @method static \Mini\Database\Mysql\Query\Builder table(string $table)
+ * @method static \Mini\Database\Mysql\Query\Expression raw($value)
+ * @method static mixed selectOne(string $query, array $bindings = [])
+ * @method static array select(string $query, array $bindings = [])
+ * @method static bool insert(string $query, array $bindings = [])
+ * @method static int update(string $query, array $bindings = [])
+ * @method static int delete(string $query, array $bindings = [])
+ * @method static bool statement(string $query, array $bindings = [])
+ * @method static int affectingStatement(string $query, array $bindings = [])
+ * @method static bool unprepared(string $query)
+ * @method static array prepareBindings(array $bindings)
+ * @method static mixed transaction(\Closure $callback, int $attempts = 1)
+ * @method static void beginTransaction()
+ * @method static void commit()
+ * @method static void rollBack()
+ * @method static int transactionLevel()
+ * @method static array pretend(\Closure $callback)
+ * @package Mini\Database\Mysql
+ */
 class DB extends Manager
 {
-
 }
--- src/Database/Redis/Redis.php
+++ src/Database/Redis/Redis.php
@@ -7,29 +7,19 @@ declare(strict_types=1);
 
 namespace Mini\Database\Redis;
 
-use Mini\Singleton;
-
 /**
  * Class Redis
  * @package Mini\Database\Redis
- * @mixin \Swoole\Coroutine\Redis | \Redis
  */
 class Redis
 {
-    protected \Redis $connection;
-
-    public function __construct(string $connection = '', array $config = [])
+    public static function connection($connection = 'default'): \Redis
     {
-        $this->connection = Pool::getInstance($config)->getConnection($connection);
-    }
-
-    public function __call($name, $arguments)
-    {
-        return $this->connection->{$name}(...$arguments);
+        return Pool::getInstance()->getConnection($connection);
     }
 
     public static function __callStatic($name, $arguments)
     {
-        return (new self())->connection->{$name}(...$arguments);
+        return Pool::getInstance()->getConnection('default')->{$name}(...$arguments);
     }
 }
