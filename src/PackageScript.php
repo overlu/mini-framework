c15d1c40ffce104915bde4c92aa9894bc2bbb2bd
lupeng <lupeng@admin.com>
2020-12-20 20:38:01 +0800

    add script

---
+++ src/PackageScript.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Mini.
+ * @auth lupeng
+ */
+declare(strict_types=1);
+
+namespace Mini;
+
+use Composer\Script\Event;
+
+/**
+ * Class PackageScript
+ * @package Mini
+ */
+class PackageScript
+{
+    /**
+     * @param Event $event
+     */
+    public static function postRootPackageInstall(Event $event)
+    {
+        file_exists('.env') || @copy('.env.example', '.env');
+    }
+
+    /**
+     * @param Event $event
+     */
+    public static function postAutoloadDump(Event $event)
+    {
+        file_exists('vendor/laravel') || @mkdir('vendor/laravel', 0644, true);
+    }
+}
