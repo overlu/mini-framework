4cf9d80922a23d9c1d857fbe39d2ad60bb88fc43
lupeng <lupeng@admin.com>
2020-12-27 15:49:10 +0800

    update to 1.3.1

--- src/Application.php
+++ src/Application.php
@@ -21,7 +21,7 @@ use Mini\Support\Command;
 
 class Application
 {
-    public static string $version = '1.3.0';
+    public static string $version = '1.3.1';
 
     public static array $mapping = [
         'http' => HttpServer::class,
