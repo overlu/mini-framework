4bac841536a8e4c7030f03ef6494aa9bd120fe19
lu <lu@peng.com>
2021-01-04 17:48:21 +0800

    update

--- src/Application.php
+++ src/Application.php
@@ -21,7 +21,7 @@ use Mini\Support\Command;
 
 class Application
 {
-    public static string $version = '1.3.3';
+    public static string $version = '1.3.4';
 
     public static array $mapping = [
         'http' => HttpServer::class,
