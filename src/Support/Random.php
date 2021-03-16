7190744420cbdb7fc4db5d56dc66aa1f02356596
lu <lu@peng.com>
2021-03-16 17:54:23 +0800

    clean

--- src/Support/Random.php
+++ src/Support/Random.php
@@ -10,35 +10,33 @@ namespace Mini\Support;
 class Random
 {
     /**
-     * 生成随机字符串 可用于生成随机密码等
-     * @param int $length 生成长度
-     * @param string $alphabet 自定义生成字符集
-     * @return bool|string
-     * @author : evalor <master@evalor.cn>
+     * @param int $length
+     * @param string $alphabet
+     * @return false|string
      */
     public static function character($length = 6, $alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789')
     {
         mt_srand();
-        // 重复字母表以防止生成长度溢出字母表长度
         if ($length >= strlen($alphabet)) {
             $rate = (int)($length / strlen($alphabet)) + 1;
             $alphabet = str_repeat($alphabet, $rate);
         }
-        // 打乱顺序返回
         return substr(str_shuffle($alphabet), 0, $length);
     }
 
     /**
-     * 生成随机数字 可用于生成随机验证码等
-     * @param int $length 生成长度
-     * @return bool|string
-     * @author : evalor <master@evalor.cn>
+     * @param int $length
+     * @return false|string
      */
     public static function number($length = 6)
     {
         return static::character($length, '0123456789');
     }
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public static function arrayRandOne(array $data)
     {
         mt_srand();
