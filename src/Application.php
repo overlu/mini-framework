6b0771592af1496c03bdaec0103d5da6e6c7d8e8
lupeng <lupeng.me@gmail.com>
2022-07-04 16:03:31 +0800

    fix

--- src/Application.php
+++ src/Application.php
@@ -27,7 +27,7 @@ class Application
      * version
      * @var string
      */
-    public static string $version = '2.13.7';
+    public static string $version = '2.13.8';
 
     /**
      * @var array|string[]
